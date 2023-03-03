0331c645bbdf0e43f1413f4ee45bcf349764474a
patrickdemurtas <patrickmurtass@gmail.com>
2023-03-03 16:01:45 -0300

    test4: ranking

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -6,14 +6,12 @@ try {
 
     const lastTen = await db.query(`
 
-      SELECT users.id, users.name,
-      COALESCE(COUNT(urls.id),0) AS "linksCount"
-      COALESCE(SUM("urls.visitCount"),0) AS "visitCount"
-      FROM users
-      LEFT JOIN urls ON users.id = "urls.userId"
+      SELECT users.id, users.name, COUNT(urls.id) AS "linksCount", SUM(urls."visitCount") AS "visitCount"
+      FROM users 
+      JOIN urls ON users.id = urls."userId"
       GROUP BY users.id
-      ORDER BY "visitCount" DESC 
-      LIMIT 10
+      ORDER BY "visitCount" DESC
+      LIMIT 10; 
 `);
 
 return res.status(200).send(lastTen.rows)
