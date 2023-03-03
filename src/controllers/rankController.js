b752333b7d6d5aee48eae9eff5d223a0109883b9
patrickdemurtas <patrickmurtass@gmail.com>
2023-03-03 15:38:15 -0300

    test2: ranking

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -8,11 +8,12 @@ try {
 
       SELECT users.id, users.name,
       COALESCE(COUNT(urls.id),0) AS "linksCount"
-      COALESCE(SUM(urls."visitCount"),0) AS "visitCount"
+      COALESCE(SUM(urls.visitCount),0) AS "visitCount"
       FROM users
-      LEFT JOIN urls ON users.id = urls."userId"
+      LEFT JOIN urls ON users.id = urls.userId
       GROUP BY users.id
-      ORDER BY "visitCount" DESC LIMIT 10
+      ORDER BY "visitCount" DESC 
+      LIMIT 10
 `);
 
 return res.status(200).send(lastTen.rows)
