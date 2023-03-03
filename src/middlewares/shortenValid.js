bba9a2d269187e8383bd6049576a89a4b9e2c16a
patrickdemurtas <patrickmurtass@gmail.com>
2023-03-03 05:30:01 -0300

    test5: shortenUrl

--- src/middlewares/shortenValid.js
+++ src/middlewares/shortenValid.js
@@ -9,12 +9,19 @@ export async function shortenedUrlValidation(req, res, next) {
 
     const url = req.body;
 
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace("Bearer ", "");
+
+
     const { error } = shortenedUrlSchema.validate(url, { abortEarly: false });
 
     if (error) {
         const errorsMessage = error.details.map((dt) => dt.message);
         return res.status(422).send(errorsMessage);
     }
+
+    res.locals.token = token;
 
     next();
 }
