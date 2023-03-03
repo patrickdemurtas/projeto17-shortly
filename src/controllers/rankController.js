689da0cce179e7e9988497781ea7199f3de70da4
patrickdemurtas <patrickmurtass@gmail.com>
2023-03-03 15:44:54 -0300

    test3: ranking

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -8,9 +8,9 @@ try {
 
       SELECT users.id, users.name,
       COALESCE(COUNT(urls.id),0) AS "linksCount"
-      COALESCE(SUM(urls.visitCount),0) AS "visitCount"
+      COALESCE(SUM("urls.visitCount"),0) AS "visitCount"
       FROM users
-      LEFT JOIN urls ON users.id = urls.userId
+      LEFT JOIN urls ON users.id = "urls.userId"
       GROUP BY users.id
       ORDER BY "visitCount" DESC 
       LIMIT 10
